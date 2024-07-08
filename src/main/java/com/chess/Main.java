52231eb67b2eb118983f9f28db95c216f0c5097a
Marcos Júnior <marcosajr11@gmail.com>
2024-07-08 19:52:19 -0300

    feat: Moving pieces

--- src/main/java/com/chess/Main.java
+++ src/main/java/com/chess/Main.java
@@ -1,12 +1,29 @@
 package com.chess;
 
 import com.chess.model.chess.ChessMatch;
+import com.chess.model.chess.ChessPiece;
+import com.chess.model.chess.ChessPosition;
 import com.chess.view.UI;
+
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
         ChessMatch chessMatch = new ChessMatch();
-        UI.printBoard(chessMatch.getPieces());
+
+        while (true) {
+            UI.printBoard(chessMatch.getPieces());
+            System.out.println();
+            System.out.print("Source: ");
+            ChessPosition source = UI.readChessPosition(sc);
+
+            System.out.println();
+            System.out.print("Target: ");
+            ChessPosition target = UI.readChessPosition(sc);
+
+            ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+        }
     }
 }
--- src/main/java/com/chess/model/chess/ChessException.java
+++ src/main/java/com/chess/model/chess/ChessException.java
@@ -1,6 +1,8 @@
 package com.chess.model.chess;
 
-public class ChessException extends RuntimeException {
+import com.chess.model.boardgame.BoardException;
+
+public class ChessException extends BoardException  {
 
     public ChessException(String msg){
         super(msg);
